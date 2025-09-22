05a90ebfdc38e082f65706b8bea472f233783695
chadheimaraofficial-creator <chadheimaraofficial@gmail.com>
2025-09-22 11:54:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// When the button is clicked, show a message
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("galleryBtn");
+
+  button.addEventListener("click", function () {
+    alert("🌾 Welcome to CHADHEIMARA ! Enjoy our gallery.");
+  });
+});
